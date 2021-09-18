333b8272dd2b22907e4285ad50e66bf888979f60
Euhid Aman <aman.derax20@gmail.com>
2021-09-18 21:57:51 +0530

    completed till pattern12

---
+++ 1. Basics of Programming/Getting Started/eachDigit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class eachDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        // prints each individual digit in reverse order
+        // while (num != 0) {
+        // int r = num % 10;
+        // System.out.println(r);
+        // num /= 10;
+        // }
+
+    }
+}
---
+++ trial.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class trial {
+
+    public static void main(String[] args) {
+        System.out.println(3 / 2);
+    }
+}
