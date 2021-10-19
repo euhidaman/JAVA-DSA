e3f0d0f06042debb491dbb293c727266ae331a85
Euhid Aman <aman.derax20@gmail.com>
2021-10-19 09:45:11 +0530

    Completed rotate an array

--- PC/trial.java
+++ PC/trial.java
@@ -4,8 +4,8 @@ public class trial {
 
     public static void main(String[] args) {
         // System.out.println(3 / 2);
-        int a = -1;
-        a = -(a);
-        System.out.println(a);
+        int a = -7;
+        // a = -(a);
+        System.out.println(a % 5);
     }
 }
