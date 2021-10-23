b960d802201c5c261576a53e4e6249b1d3f15094
Euhid Aman <aman.derax20@gmail.com>
2021-10-23 13:27:41 +0530

    fixed prime number program

--- PC/1. Basics of Programming/Getting Started/PrimeChecker.java
+++ PC/1. Basics of Programming/Getting Started/PrimeChecker.java
@@ -1,10 +1,10 @@
 // java program to check if a number is prime or not
 public class PrimeChecker {
     public static void main(String[] args) {
-        int num = 16;
+        int num = 31;
 
         boolean isPrime = true;
-        for (int i = 2; i < Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 isPrime = false;
                 break;
--- PC/trial.java
+++ PC/trial.java
@@ -3,9 +3,22 @@ import java.util.*;
 public class trial {
 
     public static void main(String[] args) {
-        // System.out.println(3 / 2);
-        int a = -7;
-        // a = -(a);
-        System.out.println(a % 5);
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        for (int i = num1; i <= num2; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+                System.out.println(i);
+        }
     }
 }
