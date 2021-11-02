e7b0b376bc9af700714fedead4c590c1ac9918bd
Euhid Aman <aman.derax20@gmail.com>
2021-11-02 22:53:26 +0530

    Completed exit point of matrix

--- PC/1. Basics of Programming/2D-arrays/exitPointOfMatrix.java
+++ PC/1. Basics of Programming/2D-arrays/exitPointOfMatrix.java
@@ -6,5 +6,45 @@ public class exitPointOfMatrix {
 
         int row = sc.nextInt();
         int column = sc.nextInt();
+
+        int arr[][] = new int[row][column];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        int i = 0, j = 0, dir = 0;
+        while (true) {
+            dir = (dir + arr[i][j]) % 4;
+
+            if (dir == 0) {
+                j++;
+            } else if (dir == 1) {
+                i++;
+            } else if (dir == 2) {
+                j--;
+            } else if (dir == 3) {
+                i--;
+            }
+
+            if (i < 0) {
+                i++;
+                break;
+            } else if (j < 0) {
+                j++;
+                break;
+            } else if (i == column) {
+                i--;
+                break;
+            } else if (j == row) {
+                j--;
+                break;
+            }
+        }
+
+        System.out.println(i);
+        System.out.println(j);
     }
 }
