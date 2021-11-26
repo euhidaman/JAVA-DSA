91f90fb5f795373b26ab54720b1181f772b9083c
Euhid Aman <aman.derax20@gmail.com>
2021-11-26 22:46:50 +0530

    Completed Codechef/Alternative Square Pattern

---
+++ Extras/Codechef/SQALPAT.java
@@ -0,0 +1,39 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[][] arr = new int[N][5];
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < 5; j++) {
+                    count++;
+                    arr[i][j] = count;
+                }
+            } else {
+                for (int j = 4; j >= 0; j--) {
+                    count++;
+                    arr[i][j] = count;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
