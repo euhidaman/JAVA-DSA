47bb8270acb2dc28f5e3cb9786a014deb451e2fd
Euhid Aman <aman.derax20@gmail.com>
2021-11-26 21:33:57 +0530

    Finished Codewars/humanReadableTime

---
+++ Extras/Codewars/humanReadableTime.java
@@ -0,0 +1,31 @@
+
+public class humanReadableTime {
+
+    public static String makeReadable(int seconds) {
+        // Do something
+
+        int anHourInSeconds = 3600;
+        int aMinuteInSeconds = 60;
+
+        String hh = Integer.toString((seconds / anHourInSeconds));
+        String mm = Integer.toString((seconds % anHourInSeconds) / aMinuteInSeconds);
+        String ss = Integer.toString(seconds % aMinuteInSeconds);
+
+        if (hh.length() == 1) {
+            hh = "0" + hh;
+        }
+        if (mm.length() == 1) {
+            mm = "0" + mm;
+        }
+        if (ss.length() == 1) {
+            ss = "0" + ss;
+        }
+
+        return hh + ":" + mm + ":" + ss;
+    }
+
+    public static void main(String args[]) {
+        int seconds = 86399;
+        System.out.println(makeReadable(seconds));
+    }
+}
