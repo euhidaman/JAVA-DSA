65b2fcc378e9d50d0cfb6a1e78175e115ef9f38f
Euhid Aman <aman.derax20@gmail.com>
2021-12-05 00:20:37 +0530

    Changed saddle point code

--- PC/1. Basics of Programming/2D-arrays/saddlePrice.java
+++ PC/1. Basics of Programming/2D-arrays/saddlePrice.java
@@ -1,52 +1,43 @@
 import java.util.*;
 
-//least point in row, but
+//least point in row
 //max point in column
-
 public class saddlePrice {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        int arr[][] = new int[n][n];
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                arr[i][j] = sc.nextInt();
+                arr[i][j] = scn.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]+" ");
-            }
-            System.out.println();
-        }
-
-        int max = Integer.MIN_VALUE, row = -1, col=-1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if(arr[i][j] > max)
-                {
-                    max = arr[i][j];
-                    row = i;
-                    col = j;
+        int colno = 0, min = 0;
+        for (int i = 0; i < n; i++) {   // rows
+            min = Integer.MAX_VALUE;
+            for (int j = 0; j < n; j++) {   // columns
+                if (arr[i][j] < min) {
+                    min = arr[i][j];
+                    colno = j;
                 }
             }
-        }
-
-        int min=Integer.MAX_VALUE, new_col=-1;
-        for (int j = 0; j < n; j++) {
-            if(arr[row][j]<min)
-            {
-                min = arr[row][j];
-                new_col = j;
+            
+            boolean potentialAns = true;
+            for (int r = 0; r < n; r++) {
+                if (min < arr[r][colno]) {
+                    potentialAns = false;
+                    break;
+                }
+            }
+            if (potentialAns) {
+                System.out.print(min);
+                return;
             }
         }
 
-        if(new_col > col)
-            System.out.println("Invalid input");
-        else
-            System.out.println(min);
+        System.out.print("Invalid Input");
     }
 }
