7c587937a53832a11f17d79aaf44c3722b53f125
Euhid Aman <aman.derax20@gmail.com>
2021-12-04 21:39:21 +0530

    Completed state of wakanda 2

--- PC/1. Basics of Programming/2D-arrays/stateOfWakanda2.java
+++ PC/1. Basics of Programming/2D-arrays/stateOfWakanda2.java
@@ -4,5 +4,21 @@ public class stateOfWakanda2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
+        int arr[][] = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int gap=0; gap<arr.length; gap++)
+        {
+            for (int i = 0, j=gap;  j<arr.length ; i++,j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+
     }
 }
