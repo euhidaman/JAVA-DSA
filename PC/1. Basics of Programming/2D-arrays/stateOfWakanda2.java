7ddf61c4d6214ac32836652f3033e0b73bd9ac20
Euhid Aman <aman.derax20@gmail.com>
2021-12-04 16:38:24 +0530

    Dummy commit for terminal access

--- PC/1. Basics of Programming/2D-arrays/stateOfWakanda2.java
+++ PC/1. Basics of Programming/2D-arrays/stateOfWakanda2.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class stateOfWakanda2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
     }
 }
