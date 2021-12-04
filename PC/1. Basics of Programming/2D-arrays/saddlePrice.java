9df2627aa929c53d71694423b00bc12ebd4ec011
Euhid Aman <aman.derax20@gmail.com>
2021-12-04 23:08:15 +0530

    Added description of saddle point

--- PC/1. Basics of Programming/2D-arrays/saddlePrice.java
+++ PC/1. Basics of Programming/2D-arrays/saddlePrice.java
@@ -1,5 +1,8 @@
 import java.util.*;
 
+//least point in row, but
+//max point in column
+
 public class saddlePrice {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,6 +14,13 @@ public class saddlePrice {
             for (int j = 0; j < n; j++) {
                 arr[i][j] = sc.nextInt();
             }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
 
         int max = Integer.MIN_VALUE, row = -1, col=-1;
