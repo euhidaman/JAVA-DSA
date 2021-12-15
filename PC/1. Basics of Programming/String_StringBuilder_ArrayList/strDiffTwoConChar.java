6d2266d74d290dad899663644881bab8270574cc
euhidaman <aman.derax20@gmail.com>
2021-12-15 20:26:03 +0530

    Added num array

--- PC/1. Basics of Programming/String_StringBuilder_ArrayList/strDiffTwoConChar.java
+++ PC/1. Basics of Programming/String_StringBuilder_ArrayList/strDiffTwoConChar.java
@@ -4,7 +4,11 @@ public class strDiffTwoConChar {
 
     public static String solution(String str){
         // write your code here
-
+        StringBuilder res = new StringBuilder(str);
+        int[] num = new int[res.length()-1];
+        for (int i = 1; i < res.length(); i++) {
+            char cur = res.charAt(i-1);
+        }
         return null;
     }
     public static void main(String[] args) {
