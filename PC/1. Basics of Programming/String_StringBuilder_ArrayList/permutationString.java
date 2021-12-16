76169f55318e1be5ad2ed19fdf1a0e093f11a7df
euhidaman <aman.derax20@gmail.com>
2021-12-16 23:20:40 +0530

    Completed all permutations of strings

--- PC/1. Basics of Programming/String_StringBuilder_ArrayList/permutationString.java
+++ PC/1. Basics of Programming/String_StringBuilder_ArrayList/permutationString.java
@@ -1,11 +1,32 @@
-import java.io.*;
 import java.util.*;
 
-public class Main {
+public class permutationString {
 
     public static void solution(String str){
         // write your code here
+        int n = str.length();
+        int fact = factorial(n);
 
+        for (int i = 0; i < fact; i++) {
+            StringBuilder res = new StringBuilder(str);
+            int temp = i;
+            for (int div = n; div >= 1 ; div--) {
+                int r = temp%div;
+                System.out.print(res.charAt(r));
+                res.deleteCharAt(r);
+                temp/=div;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int factorial(int n){
+        int val = 1;
+        for (int i = 2; i <= n; i++) {
+            val*=i;
+        }
+        return val;
     }
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
